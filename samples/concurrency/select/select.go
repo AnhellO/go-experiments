5d0f41c8670acb160dca5059c48e863db8017360
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2020-07-17 19:25:09 -0500

    feat(concurrency): add new samples

---
+++ samples/concurrency/select/select.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("I'm listening")
+
+	c := fanIn(boring("Ann"), boring("Joe"))
+	for i := 0; i < 20; i++ {
+		fmt.Println(<-c)
+	}
+
+	fmt.Println("You're both boring; I'm leaving")
+}
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
+		}
+	}()
+
+	return c
+}
