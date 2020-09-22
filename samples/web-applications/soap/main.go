21cd47e43961f968b5b638acafb90cf247b2dd61
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2020-09-22 01:45:40 -0500

    chore(soap): make client work

--- samples/web-applications/soap/main.go
+++ samples/web-applications/soap/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
 
 	calculator "gitlab.com/AnhellO/go-experiments/samples/web-applications/soap/calculator"
 
@@ -10,20 +14,31 @@ import (
 
 func main() {
 	client := soap.Client{
-		URL:         "http://www.dneonline.com/calculator.asmx",
-		Namespace:   calculator.Namespace,
-		Header:      "SOAPAction: \"http://tempuri.org/Add\"",
-		ContentType: "application/soap+xml; charset=utf-8;",
+		URL:           "http://www.dneonline.com/calculator.asmx",
+		Namespace:     calculator.Namespace,
+		URNamespace:   calculator.Namespace,
+		ThisNamespace: calculator.Namespace,
+		Envelope:      "http://www.w3.org/2003/05/soap-envelope",
+		Pre: func(r *http.Request) {
+			buf := new(strings.Builder)
+			_, err := io.Copy(buf, r.Body)
+
+			if err != nil {
+				fmt.Errorf("Error: %v", err)
+			}
+
+			updatedBody := strings.Replace(buf.String(), "<Add>", "<Add xmlns=\"http://tempuri.org/\">", -1)
+			r.Body = ioutil.NopCloser(strings.NewReader(updatedBody))
+			r.ContentLength = int64(len(updatedBody))
+		},
 	}
 
 	soapService := calculator.NewCalculatorSoap(&client)
 	response, err := soapService.Add(&calculator.Add{IntA: 5, IntB: 5})
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Errorf("Error: %v", err)
 	}
 
-	fmt.Println(client)
-	fmt.Printf("%v\n", soapService)
-	fmt.Println(response)
+	fmt.Println(*response)
 }
