27a27959636868a7a8af3851333d097cb3d0749d
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2020-10-14 19:32:26 -0500

    feat(concurrency): add parallel go-routines sample

---
+++ samples/concurrency/parallel/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+func checkExpire() {
+	for {
+		// do some job
+		fmt.Println(time.Now().UTC())
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
+
+func main() {
+	go checkExpire()
+	http.HandleFunc("/", handler) // http://127.0.0.1:8080/Go
+	http.ListenAndServe(":8080", nil)
+}
