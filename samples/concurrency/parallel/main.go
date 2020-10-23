d8fe1fcb3853bc9471cf31583166e220f18e8432
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2020-10-23 17:33:34 -0500

    feat(web): add new sample for templates handling

--- samples/concurrency/parallel/main.go
+++ samples/concurrency/parallel/main.go
@@ -9,6 +9,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
+
 func checkExpire() {
 	for {
 		// do some job
---
+++ samples/web-applications/templates/README.md
@@ -0,0 +1,3 @@
+# HTML Templates
+
+Sample taken from <https://gowebexamples.com/templates/>.
